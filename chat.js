d96638dc5311838ab2dd00a868e49ca84b0cf246
nwaguvictor <leofizzy3@gmail.com>
2022-01-09 22:08:15 +0100

    refactoring the http server

--- chat.js
+++ chat.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const http = require('http')
+const {createServer} = require('http')
 const { Server } = require('socket.io');
 
 const app = express()
 
 app.use(express.static(__dirname + '/public'));
 
-const server = http.createServer(app)
+const server = createServer(app)
 
 // Socket.io server
 const io = new Server(server, {
@@ -17,10 +17,14 @@ const io = new Server(server, {
 
 io.on('connection', socket => {
   socket.on('welcome', () => console.log(socket.id + ' joined'));
+  socket.on('disconnecting', () => {
+    console.log(socket.id + " is about to leave the group");
+  })
 
   socket.on('message:sent', msg => {
     socket.broadcast.emit('message:received', {text: msg.text})
   })
 })
+
 
 server.listen(9000);
